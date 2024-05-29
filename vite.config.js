ca814ab0d1bddad25c814a2ab6ed8808ce48965f
AniketKhonde <aniketkhonde51@gmail.com>
2024-05-29 22:11:48 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-  },
+  server: {
+    host: '0.0.0.0', // Add this line
+    port: 3000       // Optional: specify a port
+  }
 });
